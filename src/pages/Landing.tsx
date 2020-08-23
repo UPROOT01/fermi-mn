b708414a4c511e07358e4e23dfe530929aaa5177
Andri <aleksanderandrislr@gmail.com>
2020-08-23 23:03:13 +0200

    landing

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import "./Landing.css";
+import logo from "./logo.png";
 
 interface Props {}
 
 const LandingPage: React.FC<Props> = () => {
-	return <main className="Landing"></main>;
+	return (
+		<main className="Landing">
+			<div className="background">
+				<img src={logo} alt="error" className="logo" />
+				<div className="titles">
+					<h1>Benvenuti al Fermi!</h1>
+					<h2>Istituto superiore “E. Fermi”</h2>
+				</div>
+			</div>
+		</main>
+	);
 };
 
 export default LandingPage;

b4b32e9b39e6973f4e9bb61118777b2705c43d7b
algoram <enricomilanese913@gmail.com>
2020-08-25 00:36:27 +0200

    Aggiunta header bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,14 @@ import "./App.css";
 import LandingPage from "./pages/Landing";
 import Slider from "./components/Slider";
 import Article from "./components/Article";
+import HeaderBar from "./components/HeaderBar";
 
 function App() {
 	return (
-		<div style={{ width: "60%" }}>
-			<Article
-				title="ULTIMO GIORNO DI SCUOLA"
-				description="In questo strano ultimo giorno di scuola ci tenevo a salutare tutte le ragazze e i ragazzi, sono orgogliosa di voi perchè quasi tutti avete affrontato questo lungo periodo con grande serietà..."
-				imageUrl="https://www.fermimn.edu.it/?clean=true&action=icon&newsid=752"
-			></Article>
+		<div>
+			<HeaderBar></HeaderBar>
+			<div className="test"></div>
+			<p>fine</p>
 		</div>
 	);
 }

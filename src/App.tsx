034188b3b924b3e1b5e8c6e492d7a208421ceb19
Andri <aleksanderandrislr@gmail.com>
2020-08-24 23:24:51 +0200

    swipe

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,7 @@ import Slider from "./components/Slider";
 import Article from "./components/Article";
 
 function App() {
-	return (
-		<div style={{ width: "60%" }}>
-			<Article
-				title="ULTIMO GIORNO DI SCUOLA"
-				description="In questo strano ultimo giorno di scuola ci tenevo a salutare tutte le ragazze e i ragazzi, sono orgogliosa di voi perchè quasi tutti avete affrontato questo lungo periodo con grande serietà..."
-				imageUrl="https://www.fermimn.edu.it/?clean=true&action=icon&newsid=752"
-			></Article>
-		</div>
-	);
+	return <Slider></Slider>;
 }
 
 export default App;

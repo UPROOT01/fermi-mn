e7c9f8e6ba0e26b6fc9eeb273f6c582a20e354bc
Andri <aleksanderandrislr@gmail.com>
2020-08-26 18:12:33 +0200

    bug-fix + generalizzazione dell'header

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,10 @@ import HeaderBar from "./components/HeaderBar";
 import HomePage from "./pages/Home";
 import AboutUsPage from "./pages/AboutUs";
 import UsefulLinksPage from "./pages/UsefulLinks";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-	return <UsefulLinksPage></UsefulLinksPage>;
+	return <BrowserRouter></BrowserRouter>;
 }
 
 export default App;
--- src/components/HeaderBar.tsx
+++ src/components/HeaderBar.tsx
@@ -2,17 +2,49 @@ import React, { useState } from "react";
 import logoOutline from "../assets/logo_outline.svg";
 import logoText from "../assets/logo_text.svg";
 import "./HeaderBar.css";
+import { link } from "fs";
 
 interface Props {}
 
 const links: {
 	title: string;
 	link?: string;
-	child?: { open: boolean; children: { title: string; link: string }[] };
-}[] = [];
+	child?: { children: { title: string; link: string }[] };
+}[] = [
+	{ title: "Home" },
+	{
+		title: "Settings",
+		child: {
+			children: [
+				{ title: "heyla", link: "" },
+				{ title: "heyla", link: "" },
+				{ title: "heyla", link: "" },
+				{ title: "heyla", link: "" },
+			],
+		},
+	},
+];
+
+const isLastOpen = (array: { [index: number]: boolean }) => {
+	if (array[links.length - 1] === undefined) {
+		return false;
+	}
+
+	return array[links.length - 1];
+};
 
 const HeaderBar: React.FC<Props> = () => {
-	const [activeMenus, setActiveMenus] = useState(links);
+	const [linksState, setLinksState] = useState(() => {
+		const obj: { [index: number]: boolean } = {};
+
+		links.forEach((link, index) => {
+			if (link.child !== undefined) {
+				obj[index] = false;
+			}
+		});
+
+		return obj;
+	});
 	const [mobileMenu, setMobileMenu] = useState(false);
 
 	return (
@@ -22,89 +54,46 @@ const HeaderBar: React.FC<Props> = () => {
 					<img className="header-logo-img" src={logoOutline} alt="error" />
 					<img className="header-logo-text" src={logoText} alt="error" />
 				</div>
-				<span>Home</span>
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([!activeMenus[0], false, false, false])}
-				>
-					Area riservata
-				</span>
-				<span
-					onClick={() => setActiveMenus([!activeMenus[0], false, false, false])}
-					className={activeMenus[0] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, !activeMenus[1], false, false])}
-				>
-					Istituto
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, !activeMenus[1], false, false])}
-					className={activeMenus[1] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<span className="header-spaced">Reti di ambito</span>
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, false, !activeMenus[2], false])}
-				>
-					A.S. 2019/2020
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, false, !activeMenus[2], false])}
-					className={activeMenus[2] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, false, false, !activeMenus[3]])}
-				>
-					I nostri siti
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, false, false, !activeMenus[3]])}
-					className={activeMenus[3] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<span className="header-spaced">Link utili</span>
+				{links.map((link, index) => {
+					if (link.child === undefined) {
+						return <span>{link.title}</span>;
+					} else {
+						return (
+							<>
+								<span
+									className="header-spaced"
+									onClick={() =>
+										setLinksState({
+											...linksState,
+											[index]: !linksState[index],
+										})
+									}
+								>
+									{link.title}
+								</span>
+								<span
+									onClick={() =>
+										setLinksState({
+											...linksState,
+											[index]: !linksState[index],
+										})
+									}
+									className={linksState[index] ? "header-rotated" : ""}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+									>
+										<path d="M0 0h24v24H0z" fill="none" />
+										<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+									</svg>
+								</span>
+							</>
+						);
+					}
+				})}
 				<span
 					className="header-hamburger"
 					onClick={() => setMobileMenu(!mobileMenu)}
@@ -120,214 +109,73 @@ const HeaderBar: React.FC<Props> = () => {
 					</svg>
 				</span>
 			</div>
-
 			<div
 				className={
-					"header-mobile-menu" + (mobileMenu ? "" : " header-mobile-hidden")
+					"header-mobile-menu" +
+					(mobileMenu ? "" : " header-mobile-hidden") +
+					(isLastOpen(linksState) ? "" : " header-mobile-lastopen")
 				}
 			>
-				<span className="header-spaced">Home</span>
-				<br />
-				<br />
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([!activeMenus[0], false, false, false])}
-				>
-					Area riservata
-				</span>
-				<span
-					onClick={() => setActiveMenus([!activeMenus[0], false, false, false])}
-					className={activeMenus[0] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<br />
-				<div
-					id="header-area-riservata"
-					className={activeMenus[0] ? "header-menu" : "header-hidden"}
-				>
-					<span>Studenti</span>
-					<br />
-					<br />
-					<span>Genitori</span>
-					<br />
-					<br />
-					<span>Docenti</span>
-					<br />
-					<br />
-					<span>Personale A.T.A</span>
-				</div>
-				<br />
-
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, !activeMenus[1], false, false])}
-				>
-					Istituto
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, !activeMenus[1], false, false])}
-					className={activeMenus[1] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<br />
-				<div
-					id="header-istituto"
-					className={activeMenus[1] ? "header-menu" : "header-hidden"}
-				>
-					<span>Chi siamo</span>
-					<br />
-					<br />
-					<span>Le persone</span>
-					<br />
-					<br />
-					<span>I nostri corsi</span>
-					<br />
-					<br />
-					<span>Strutture e Servizi</span>
-					<br />
-					<br />
-					<span>Documenti</span>
-					<br />
-					<br />
-					<span>Parlano di noi</span>
-					<br />
-					<br />
-					<span>Bandi e Gare</span>
-				</div>
-				<br />
-
-				<span className="header-spaced">Reti di ambito</span>
-				<br />
-				<br />
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, false, !activeMenus[2], false])}
-				>
-					A.S. 2019/2020
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, false, !activeMenus[2], false])}
-					className={activeMenus[2] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<br />
-				<div
-					id="header-anno-scolastico"
-					className={activeMenus[2] ? "header-menu" : "header-hidden"}
-				>
-					<span>Privacy GDPR 679/16</span>
-					<br />
-					<br />
-					<span>Calendario Orari</span>
-					<br />
-					<br />
-					<span>Progetti</span>
-					<br />
-					<br />
-					<span>Cittadinanza e Costituzione</span>
-					<br />
-					<br />
-					<span>Formazione</span>
-					<br />
-					<br />
-					<span>Programmazione comune</span>
-					<br />
-					<br />
-					<span>Alternanza Scuola Lavoro</span>
-					<br />
-					<br />
-					<span>Materiali Invalsi</span>
-					<br />
-					<br />
-					<span>Info per III medie</span>
-					<br />
-					<br />
-					<span>Orientamento in uscita</span>
-					<br />
-					<br />
-					<span>FabAcademy</span>
-					<br />
-					<br />
-					<span>Innovazioni Green</span>
-					<br />
-					<br />
-					<span>Viaggi ed uscite didattiche</span>
-				</div>
-				<br />
-
-				<span
-					className="header-spaced"
-					onClick={() => setActiveMenus([false, false, false, !activeMenus[3]])}
-				>
-					I nostri siti
-				</span>
-				<span
-					onClick={() => setActiveMenus([false, false, false, !activeMenus[3]])}
-					className={activeMenus[3] ? "header-rotated" : ""}
-				>
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						height="24"
-						viewBox="0 0 24 24"
-						width="24"
-					>
-						<path d="M0 0h24v24H0z" fill="none" />
-						<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
-					</svg>
-				</span>
-				<br />
-				<br />
-				<div
-					id="header-nostri-siti"
-					className={activeMenus[3] ? "header-menu" : "header-hidden"}
-				>
-					<span>Open Source e Freeware</span>
-					<br />
-					<br />
-					<span>I siti del Fermi</span>
-					<br />
-					<br />
-					<span>Fotovoltaico</span>
-					<br />
-					<br />
-					<span>Dati Energetici</span>
-					<br />
-					<br />
-					<span>Archivio di Stato</span>
-					<br />
-					<br />
-					<span>Scuola 21</span>
-					<br />
-					<br />
-					<span>Progetto LER</span>
-				</div>
+				{links.map((link, index) => {
+					if (link.child === undefined) {
+						return (
+							<>
+								{" "}
+								<span className="header-spaced">Home</span>
+								<br />
+								<br />
+							</>
+						);
+					} else {
+						return (
+							<>
+								<span
+									className="header-spaced"
+									onClick={() =>
+										setLinksState({
+											...linksState,
+											[index]: !linksState[index],
+										})
+									}
+								>
+									Area riservata
+								</span>
+								<span
+									onClick={() =>
+										setLinksState({
+											...linksState,
+											[index]: !linksState[index],
+										})
+									}
+									className={linksState[index] ? "header-rotated" : ""}
+								>
+									<svg
+										xmlns="http://www.w3.org/2000/svg"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+									>
+										<path d="M0 0h24v24H0z" fill="none" />
+										<path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z" />
+									</svg>
+								</span>
+								<div
+									className={
+										linksState[index] ? "header-menu" : "header-hidden"
+									}
+								>
+									{link.child.children.map((child) => (
+										<>
+											<p>
+												<span>{child.title}</span>
+											</p>
+										</>
+									))}
+								</div>
+							</>
+						);
+					}
+				})}
 			</div>
 		</div>
 	);
